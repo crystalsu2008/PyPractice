f246c9f68b0a9275ce6ee50b2486eb7e4ed785e0
Chris Su <crystalsu2008@gmail.com>
2016-10-08 02:12:24 +0800

    Update mergeSRT.py

---
+++ mergeSRT.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python
+import os,sys
+
+try:
+    srt1 = open("D:/Users/Crystal/Documents/GitHub/PyPractice-master/test2015-10-02en.srt", "r")
+except IOError:
+    print "The file don't exist, Please double check!"
+    exit()
+print 'The file mode is ',srt1.mode
+print 'The file name is ',srt1.name
+P = srt1.tell()
+print 'the postion is %d' %(P)
+
+try:
+    srt2 = open("D:/Users/Crystal/Documents/GitHub/PyPractice-master/test2015-10-02ch.srt", "r")
+except IOError:
+    print "The file don't exist, Please double check!"
+    exit()
+print 'The file mode is ',srt2.mode
+print 'The file name is ',srt2.name
+P = srt2.tell()
+print 'the postion is %d' %(P)
+
+srtout = open('D:/Users/Crystal/Documents/GitHub/PyPractice-master/out.srt', 'w')
+
+dict1, dict2 = {}, {}
+
+for EachLine in srt2:
+    try:
+        key = int(EachLine)
+    except:
+        continue
+
+    time = srt2.next()
+    nextline = srt2.next()
+    subtext = ''
+    while not nextline == '\n':
+        subtext += nextline
+        try:
+            nextline = srt2.next()
+        except:
+            break
+
+    dict2[key]=(time, subtext)
+
+for EachLine in srt1:
+    try:
+        key = int(EachLine)
+    except:
+        continue
+
+    time = srt1.next()
+    nextline = srt1.next()
+    subtext = ''
+    while not nextline == '\n':
+        subtext += nextline
+        try:
+            nextline = srt1.next()
+        except:
+            break
+    dict1[key]=(time, subtext)
+
+    srtout.write(str(key)+'\n')
+    srtout.write(dict1[key][0])
+    srtout.write(dict1[key][1])
+    try:
+        srtout.write(dict2[key][1])
+    except:
+        pass
+    srtout.write('\n')
+
+srt2.close()
+srt1.close()
+srtout.close()
